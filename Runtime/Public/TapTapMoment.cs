0a0e77ef145f1e326a3a35f8d9b4080d88e80806
lc1006 <1176168734@qq.com>
2025-06-20 17:25:08 +0800

    feat:update upm

--- Runtime/Public/TapTapMoment.cs
+++ Runtime/Public/TapTapMoment.cs
@@ -43,7 +43,7 @@ namespace TapSDK.Moment
     public class TapTapMoment
     {
 
-        public static readonly string Version = "4.6.2-beta.2";
+        public static readonly string Version = "4.6.2-beta.5";
 
         // 显示动态页面
         public static void open()
